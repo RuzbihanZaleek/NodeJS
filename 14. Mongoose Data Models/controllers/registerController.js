5cda5e7777dfeb8d12d0abe0722bf402dff7b84d
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-10-18 18:38:11 +0530

    Lesson 14. "Mongoose Data Modals" added

---
+++ 14. Mongoose Data Models/controllers/registerController.js
@@ -0,0 +1,29 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    // check for duplicate usernames in the db
+    // use exec() with async await
+    const duplicate = await User.findOne({ username: user }).exec();
+
+    if (duplicate) return res.status(409).json({ 'message': `User already exists!` }); //Conflict
+    try {
+        //encrypt the password
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+
+        //create and store the new user
+        const result = await User.create({
+            "username": user,
+            "password": hashedPwd
+        });
+        
+        res.status(201).json({ 'success': `New user ${user} created successfully!` });
+    } catch (error) {
+        res.status(500).json({ 'message': error.message });
+    }
+}
+
+module.exports = { handleNewUser };
